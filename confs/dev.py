3a1f41f210f0b1581a3df083dfbe3c8645bf916c
İsmet Acar Doğan <ismetacar_@ismetacar.local>
2018-07-09 13:07:31 +0300

    confs fixed

--- confs/dev.py
+++
@@ -1,8 +0,0 @@
-conf = {
-    "mongo_connection_string": "mongodb://192.1.4.92:27017/input",
-    "api_version": "v1",
-    "secret": "AtqVaL11EHVPEpbu2mr0yZgKXj1BnRj0",
-    "default_database": "input",
-    "management_api": "http://dev-management.dogannet.tv/api",
-    "asset_service": "http://dev-assets.dogannet.tv/img"
-}
--- confs/prod.py
+++
@@ -1,8 +0,0 @@
-conf = {
-    "mongo_connection_string": "mongodb://quark:brm_hy81@192.1.4.144,192.1.4.145,192.1.4.146/input?replicaSet=dtvrepset1",
-    "api_version": "v1",
-    "secret": "AtqVaL11EHVPEpbu2mr0yZgKXj1BnRj0",
-    "default_database": "input",
-    "management_api": "http://management.dogannet.tv/api",
-    "asset_service": "http://assets.dogannet.tv/img"
-}
