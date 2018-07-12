eea7713ddc3f3856dec440b944c502c2f3e05811
İsmet Acar Doğan <ismetacar_@ismetacar.local>
2018-07-12 16:17:39 +0300

    redis initialized

--- confs/local.py
+++ confs/local.py
@@ -4,5 +4,6 @@ conf = {
     "secret": "AtqVaL11EHVPEpbu2mr0yZgKXj1BnRj0",
     "default_database": "input",
     "management_api": "http://dev-management.dogannet.tv/api",
-    "asset_service": "http://dev-assets.dogannet.tv/img"
+    "asset_service": "http://dev-assets.dogannet.tv/img",
+    "redis_host": "192.1.3.95"
 }
