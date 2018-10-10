461503baa268807b5ad98307d5651b9ff338833c
Akın Abdullahoğlu <akinabdullahoglu@AKIN-MacBook-Pro.local>
2018-10-10 16:23:02 +0300

    config revert old version

--- confs/local.py
+++ confs/local.py
@@ -1,10 +1,9 @@
 conf = {
-    "mongo_connection_string": "mongodb://192.1.4.92/quark_v3_input",
+    "mongo_connection_string": "mongodb://localhost:27017/input",
     "api_version": "v1",
     "secret": "AtqVaL11EHVPEpbu2mr0yZgKXj1BnRj0",
-    "default_database": "quark_v3_input",
+    "default_database": "input",
     "management_api": "http://dev-management.dogannet.tv/api",
     "asset_service": "http://dev-assets.dogannet.tv/img",
-    "redis_host": "192.1.3.95",
-    "salt": "ntvdY34MzYCslPl9ATqPBcYPH9vuPZEefBvlOWVPV5o="
+    "redis_host": "192.1.3.95"
 }
